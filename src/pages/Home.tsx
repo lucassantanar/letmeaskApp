410864222be2667de7feb4755592ca4120dcf1c5
Lucas Santana <lucassice@gmail.com>
2021-06-24 12:35:11 -0300

    Crição e estilização da pagina Sala de Perguntas/respostas e componente (Código da Sala). Implementação de regras no firebase database realtime e implementação de guncionalidades

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useHistory } from 'react-router-dom'
 
-import { auth, firebase } from '../services/firebase'
+// import { auth, firebase } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -10,17 +10,37 @@ import { Button } from '../components/Button'
 import { useAuth } from '../hooks/useAuth'
 
 import '../styles/auth.scss'
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useAuth()
+  const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function hendleCreateRoom() {
     if(!user) {
       await signInWithGoogle()
     }
     history.push('/rooms/new');
+  }
+
+  async function handleJoinSubmit(event:FormEvent) {
+    event.preventDefault();
+
+    if(roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()) {
+      alert('Room does not exists.')
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`)
   }
 
   return(
@@ -38,10 +58,12 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinSubmit}>
             <input 
             type="text" 
-            placeholder="Digite o código da sala" 
+            placeholder="Digite o código da sala"
+            onChange={event => setRoomCode(event.target.value)} 
+            value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
